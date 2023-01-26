8c918127282b5fd559172acaac334041678f861a
KimJinHwan999 <wlsghks5438@gmail.com>
2023-01-27 04:54:19 +0900

    feat : JWT, Cookie 로그인 기능 추가중

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const { Account } = require('../config');
+
+router.use(cookieParser());
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.clearCookie('accountToken', eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiSldUIiwiaWQiOiJ0ZXN0MyIsImlhdCI6MTY3NDc2MjQwNSwiZXhwIjoxNjc0NzYzMzA1LCJpc3MiOiJramgifQ.hfk3-HNv93K7wWKPljBv7x4qS_LBEvc1moFOXDy_T4M, { 
+    httpOnly: true
+  });
   res.send('homepage');
 });
 
